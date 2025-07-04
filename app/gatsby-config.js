ea0d8da0b5a79da68fca936ad5b8deed43852486
Alex Kennedy <apkennedy@me.com>
2025-07-04 22:27:17 +1000

    Clean-up unused files and lock deps.

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: "Quantum Hydrogen",
-    author: "Alex Kennedy",
-    description: "A visualization of the quantum states of an electron orbiting a hydrogen atom. "
+    title: 'Quantum Hydrogen',
+    author: 'Alex Kennedy',
+    description:
+      'A visualization of the quantum states of an electron orbiting a hydrogen atom.',
   },
-  pathPrefix: "/quantum-hydrogen",
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-  ],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
 }
